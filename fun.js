c6ccfa93c5fe58ef19cc39b367e40fd467e40ca6
OlgaAbdu <olgasavik84@gmail.com>
2023-02-04 22:04:46 -0600

    M&M color Switcher added

---
+++ fun.js
@@ -0,0 +1,25 @@
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+function red() {
+    document.body.style.backgroundColor = "red";
+}
+
+function orange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function brown() {
+    document.body.style.backgroundColor = "brown";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
+function violet() {
+    document.body.style.backgroundColor = "violet";
+}
